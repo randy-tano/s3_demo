96d4b8554ba0d93ae293cbc2666b2f7ba6c6ac79
randy-tano <btano@plos.org>
2018-01-08 16:54:12 -0800

    Removed AWS API KEY

--- plos/doublehelix/settings/base.py
+++ plos/doublehelix/settings/base.py
@@ -49,8 +49,6 @@ MIDDLEWARE = (
   'django.contrib.auth.middleware.AuthenticationMiddleware',
   'django.contrib.messages.middleware.MessageMiddleware',
   'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-  'plos.doublehelix.s3_connect.middleware.S3PreFlightMiddleware',
 )
 
 TEMPLATES = (
