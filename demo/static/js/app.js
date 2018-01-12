4ddafa78fb451c4ab71398d4e69d3eb6c04bafb1
randy-tano <btano@plos.org>
2018-01-12 11:22:26 -0800

    Added muti-select file uploading.

--- demo/static/js/app.js
+++ demo/static/js/app.js
@@ -8,6 +8,23 @@ var s3 = new AWS.S3({
   params: {Bucket: awsConfig.bucketName}
 });
 
+var upload_timer;
+var upload_counter = 0;
+var to_be_processed = 0;
+
+function waitForCompletion() {
+  if (upload_counter < to_be_processed) {
+	  console.log('File uploads not yet done...');
+	  upload_timer = setTimeout(waitForCompletion, 2000);
+  } else  {
+    if (upload_timer) {
+      clearTimeout(upload_timer);
+      upload_timer = 0;
+	}
+    console.log('Done with file upload!');
+  }
+}
+
 function uploadFile() {
   var files = document.getElementById('file_upload').files;
   if (!files.length) {
@@ -19,17 +36,29 @@ function uploadFile() {
 	  folderName = 'demo';
   }
 
-  var file = files[0];
-  var fileName = file.name;
-  var folderKey = encodeURIComponent(folderName) + '/';
-  var fileKey = folderKey + fileName;
-  s3.upload({
-      Key: fileKey,
-      Body: file,
-    }, function(err, data) {
-         if (err) {
-           return alert('There was an error uploading your file: ', err.message);
-         }
-         alert('Successfully uploaded file.');
-  });
+  upload_counter = 0;
+  to_be_processed = files.length;
+
+  upload_timer = setTimeout(waitForCompletion, 2000);
+
+  var file;
+  for (var i = 0; i < files.length; i++) {
+	file = files[i];
+    var fileName = file.name;
+    var folderKey = encodeURIComponent(folderName) + '/';
+    var fileKey = folderKey + fileName;
+    console.log('Requesting file: ' + fileName);
+    s3.upload({
+        Key: fileKey,
+        Body: file,
+      }, function(err, data) {
+        upload_counter += 1;
+        if (err) {
+          console.log('There was an error uploading your file: ' + data.key, err.message);
+        } else {
+          console.log('Successfully uploaded file: ' + data.key);
+        }
+      });
+  }
+  console.log('Waiting for completion...');
 }
